6c727da42dab52057659337e5b2a592b7294f585
Matis André <45696697+tismatis@users.noreply.github.com>
2025-02-03 01:08:48 +0100

    patched more vars

--- FunctionObject.cs
+++ FunctionObject.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1;
 public class FunctionObject
 {
     public string Name;
-    public List<Symbol.Symbol>? Symbols = new();
+    public List<Symbol.Symbol>? Symbols = new(); // a passer en immutable
     private Symbol.Symbol[]? _symbols;
     public int MaxStackSize;
     public bool IsAsync;
@@ -32,12 +32,11 @@ public class FunctionObject
     {
         runtime.Function = this;
         Stopwatch sw = new();
-        Symbol.Symbol[] localSymbols = _symbols;
-        int length = localSymbols.Length;
+        int length = _symbols.Length;
         MaxStackSize = length;
         sw.Start();
         for (runtime.Address = 0; runtime.Address != length; runtime.Address++)
-            localSymbols[runtime.Address].Execute(runtime);
+            _symbols[runtime.Address].Execute(runtime);
         sw.Stop();
         Console.WriteLine($"Function {Name} executed in {sw.ElapsedMilliseconds}ms");
     }
@@ -47,8 +46,7 @@ public class FunctionObject
     {
         runtime.Function = this;
         Stopwatch sw = new();
-        Symbol.Symbol[] localSymbols = _symbols;
-        int length = localSymbols.Length;
+        int length = _symbols.Length;
         MaxStackSize = length;
         sw.Start();
         for (runtime.Address = 0; runtime.Address != length; runtime.Address++)
@@ -56,7 +54,6 @@ public class FunctionObject
             if (runtime.TaskWait)
             {
                 if(runtime.TaskWaitAll)
-                {
                     while (true)
                     {
                         var stackObj = runtime.Stack.Pop();
@@ -68,15 +65,12 @@ public class FunctionObject
                             break;
                         }
                     }
-                    runtime.TaskWaitAll = false;
-                }
                 else if(runtime.Stack.Pop() is Task task)
-                {
                     task.Wait();
-                }
+                runtime.TaskWaitAll = false;
                 runtime.TaskWait = false;
             }
-            localSymbols[runtime.Address].Execute(runtime);
+            _symbols[runtime.Address].Execute(runtime);
         }
         sw.Stop();
         Console.WriteLine($"Function {Name} executed in {sw.ElapsedMilliseconds}ms");
@@ -85,13 +79,10 @@ public class FunctionObject
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Execute(FunctionRuntime runtime, int defaultAddress, params object[] args)
     {
-        Symbol.Symbol[] localSymbols = _symbols;
-        int length = localSymbols.Length;
+        int length = _symbols.Length;
         MaxStackSize = length;
         for (runtime.Address = defaultAddress; runtime.Address != length; runtime.Address++)
-        {
-            localSymbols[runtime.Address].Execute(runtime);
-        }
+            _symbols[runtime.Address].Execute(runtime);
     }
 }
 
