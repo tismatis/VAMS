91959d090d5270c97a82b21faaec61b8443b7d40
Matis André <45696697+tismatis@users.noreply.github.com>
2025-02-03 20:48:14 +0100

    Splitted the project in half!

---
+++ VAMSRunner/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using ConsoleApp1;
+
+namespace VAMSRunner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+            
+        stopwatch.Start();
+            
+        VM vm;
+            
+        using (Parser parser = new Parser())
+        {
+            stopwatch.Stop();
+            Console.WriteLine("Parsing time: " + stopwatch.ElapsedMilliseconds + "ms");
+            stopwatch.Reset();
+            
+            stopwatch.Start();
+            parser.Parse("program.lasil");
+            stopwatch.Stop();
+            Console.WriteLine("Compiling time: " + stopwatch.ElapsedMilliseconds + "ms");
+            stopwatch.Reset();
+            
+            stopwatch.Start();
+            vm = parser.GenerateVM();
+            stopwatch.Stop();
+            Console.WriteLine("Generating VM time: " + stopwatch.ElapsedMilliseconds + "ms");
+            stopwatch.Reset();
+        }
+            
+            
+        stopwatch.Start();
+        vm.Execute("Program", "Main");
+        stopwatch.Stop();
+        Console.WriteLine("Execution time: " + stopwatch.ElapsedMilliseconds + "ms");
+    }
+}
