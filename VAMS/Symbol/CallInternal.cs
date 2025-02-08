556b7800567dced038a3f83bc873059376b5e268
Matis André <45696697+tismatis@users.noreply.github.com>
2025-02-08 02:23:04 +0100

    Added CALL_INTERNAL to call functions from custom programs

---
+++ VAMS/Symbol/CallInternal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1.Symbol
+{
+    public class CallInternal : Symbol
+    {
+        public string Path;
+        public string Name;
+        public bool NeedPush;
+
+        public CallInternal() : base(null) {}
+
+        public CallInternal(string[] args) : base(args)
+        {
+            if (Parser.Loading == ParserState.Loading)
+                return;
+
+            Path = args[0];
+            Name = args[1];
+            NeedPush = bool.Parse(args[2]);
+        }
+
+        public override string GetCommand() => "CALL_INTERNAL";
+        
+        public override void Execute(FunctionRuntime runtime)
+        {
+            VAMSInterface.OnConsoleOutput("Executing internal function " + Path + "." + Name);
+
+            if(NeedPush)
+                runtime.Stack.Push(runtime.Vm.Execute(Path, Name));
+            else
+                runtime.Vm.Execute(Path, Name);
+        }
+    }
+}
