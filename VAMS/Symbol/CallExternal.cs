9124a710fbdab4611759b3b3a172baff07a33cc0
Matis André <45696697+tismatis@users.noreply.github.com>
2025-02-08 02:22:45 +0100

    Added arguments + push ability to CALL_EXTERNAL

--- VAMS/Symbol/CallExternal.cs
+++ VAMS/Symbol/CallExternal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace ConsoleApp1.Symbol
@@ -7,6 +8,8 @@ namespace ConsoleApp1.Symbol
     {
         public string Path;
         public string Name;
+        private bool ShouldPush;
+        private MethodInfo _method;
 
         public CallExternal() : base(null) {}
 
@@ -17,23 +20,41 @@ namespace ConsoleApp1.Symbol
 
             Path = args[0];
             Name = args[1];
+            if (args.Length > 1)
+                ShouldPush = bool.Parse(args[2]);
+            string argsType = ""; // "Type1;Type2;Type3"
+            if(args.Length > 2)
+                argsType = args[3];
+            
+            var type = Type.GetType(Path);
+            if (type == null)
+                throw new InvalidOperationException($"Type '{Path}' not found.");
+
+            List<Type> types = new List<Type>();
+            foreach (var typeArg in argsType.Split(';'))
+            {
+                var typeArgType = Type.GetType(typeArg);
+                if (typeArgType == null)
+                    throw new InvalidOperationException($"Type '{typeArg}' not found.");
+                types.Add(typeArgType);
+            }
+            
+            if(String.IsNullOrEmpty(argsType))
+                _method = type.GetMethod(Name, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            else
+                _method = type.GetMethod(Name, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public, null, types.ToArray(), null);
+            if (_method == null)
+                throw new InvalidOperationException($"Method '{Path}.{Name}' not found.");
         }
 
         public override string GetCommand() => "CALL_EXTERNAL";
         
         public override void Execute(FunctionRuntime runtime)
         {
-            Console.WriteLine("Executing external function " + Path + "." + Name);
-
-            var type = Type.GetType(Path);
-            if (type == null)
-                throw new InvalidOperationException($"Type '{Path}' not found.");
-
-            var method = type.GetMethod(Name, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-            if (method == null)
-                throw new InvalidOperationException($"Method '{Path}.{Name}' not found.");
-
-            method.Invoke(null, new object[]{"Hello, World!"});
+            VAMSInterface.OnConsoleOutput("Executing external function " + Path + "." + Name);
+            var obj = _method.Invoke(null, new object[]{"Hello, World!"});
+            if(ShouldPush)
+                runtime.Stack.Push(obj);
         }
     }
 }
