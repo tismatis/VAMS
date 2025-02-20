94c0eee63535b1e1aa0bc2726a4b4b331b6c5659
Matis André <45696697+tismatis@users.noreply.github.com>
2025-02-20 21:34:08 +0100

    Fixed Multiply Symbol

--- VAMS/Symbol/Multiply.cs
+++ VAMS/Symbol/Multiply.cs
@@ -26,9 +26,9 @@ namespace ConsoleApp1.Symbol
             }
             
             if(type == typeof(int))
-                Execute = runtime => { runtime.Stack.Push((int)runtime.Stack.Pop() * 1); };
+                Execute = runtime => { runtime.Stack.Push((int)runtime.Stack.Pop() * (int)runtime.Stack.Pop()); };
             else if(type == typeof(float))
-                Execute = runtime => { runtime.Stack.Push((float)runtime.Stack.Pop() * 1); };
+                Execute = runtime => { runtime.Stack.Push((float)runtime.Stack.Pop() * (float)runtime.Stack.Pop()); };
             else
                 throw new InvalidOperationException("MUL NOT IMPLEMENTED FOR THIS");
         }
